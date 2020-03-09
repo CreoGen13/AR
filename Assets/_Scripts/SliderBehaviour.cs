fff4c7460f87217a05d9f68015c9e45e4bb31b9f
CreoGen13 <creogen.ac@gmail.com>
2020-03-10 00:48:09 +0300

    1
    1

---
+++ Assets/_Scripts/SliderBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderBehaviour : MonoBehaviour
+{
+    private Slider _slider;
+
+    public float Progress
+    {
+        get { return _slider.value; }
+        set
+        {
+            _slider.value = value;
+        }
+
+    }
+    public void Start()
+    {
+        _slider = GetComponent<Slider>();
+    }
+}
